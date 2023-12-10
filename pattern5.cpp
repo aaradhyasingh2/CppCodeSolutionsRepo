c6b9c2a9ffd76b34d329ae8ca1a19cb2efb2e4d3
Aaradhya Singh <153016095+aaradhyasingh2@users.noreply.github.com>
2023-12-10 10:34:56 +0530

    Create pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,41 @@
+problem:- 
+
+  #print Right Triangle pattern
+  
+    *
+   **
+  ***
+ ****
+*****
+
+solution:-
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+
+    int i=1;
+   while(i<=n){
+    int space = n-i;
+    while (space)
+    {
+        cout<<" ";
+        space--;
+    }
+    int j=1;
+    while (j<=i)
+    {
+        cout<<"*";
+        j++;
+    }
+    cout<<endl;
+    i++;
+   }
+    
+   return 0;
+}
