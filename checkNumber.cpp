7e789870c7fd58f73010c3a2e90ec0f4f8a4d2d0
Aaradhya Singh <153016095+aaradhyasingh2@users.noreply.github.com>
2023-12-15 10:07:36 +0530

    Create checkNumber.cpp

---
+++ checkNumber.cpp
@@ -0,0 +1,17 @@
+// C++ Program to check whether a number is positive or negative 
+
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+    int num; 
+    num = -100;
+    if (num >= 0) { 
+        cout << num << " is a positive number." << endl; 
+    }
+    else { 
+        cout << num << " is a negative number." << endl; 
+    } 
+    return 0; 
+}
