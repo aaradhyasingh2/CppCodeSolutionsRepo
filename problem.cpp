a6f381793d83cc8b0deee9ba1cff9c0e903c330d
Aaradhya Singh <153016095+aaradhyasingh2@users.noreply.github.com>
2024-01-26 23:25:08 +0530

    Create problem.cpp

---
+++ problem.cpp
@@ -0,0 +1,11 @@
+// program to print all natural numbers upto N without using semi-colon 
+#include <iostream> 
+
+using namespace std; 
+int main() 
+{ 
+	static int x = 1; 
+	if (cout << x << " " && x++ < N && main()) { 
+	} 
+	return 0; 
+}
