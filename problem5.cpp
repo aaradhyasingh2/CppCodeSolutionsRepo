f4707e8ab76df4a2c60d061ec968927dcbb026eb
Aaradhya Singh <153016095+aaradhyasingh2@users.noreply.github.com>
2024-02-02 14:11:06 +0530

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,15 @@
+// program to print maximum value of unsigned int. 
+
+#include <iostream>
+using namespace std; 
+
+int main() 
+{ 
+	unsigned int max; 
+	max = 0; 
+	max = ~max; 
+
+	cout << "Max value possible : " << max; 
+
+	return 0; 
+}
